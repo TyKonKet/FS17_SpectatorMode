7075b63d18d99a5f49c396aada31615df4de2907
Matteo Autieri <matteoautieri@live.it>
2017-01-12 22:50:02 +0100

    added sync of fovy

--- src/Events/actorData.lua
+++ src/Events/actorData.lua
@@ -15,7 +15,7 @@ function ActorDataEvent:emptyNew()
     return self;
 end
 
-function ActorDataEvent:new(actorName, tx, ty, tz, rx, ry, rz, rw)
+function ActorDataEvent:new(actorName, tx, ty, tz, rx, ry, rz, rw, fovy)
     local self = ActorDataEvent:emptyNew();
     self.actorName = actorName;
     self.tx = tx;
@@ -25,6 +25,7 @@ function ActorDataEvent:new(actorName, tx, ty, tz, rx, ry, rz, rw)
     self.ry = ry;
     self.rz = rz;
     self.rw = rw;
+    self.fovy = fovy;
     return self;
 end
 
@@ -37,6 +38,7 @@ function ActorDataEvent:readStream(streamId, connection)
     self.ry = streamReadFloat32(streamId);
     self.rz = streamReadFloat32(streamId);
     self.rw = streamReadFloat32(streamId);
+    self.fovy = streamReadFloat32(streamId);
     self:run(connection);
 end
 
@@ -49,6 +51,7 @@ function ActorDataEvent:writeStream(streamId, connection)
     streamWriteFloat32(streamId, self.ry);
     streamWriteFloat32(streamId, self.rz);
     streamWriteFloat32(streamId, self.rw);
+     streamWriteFloat32(streamId, self.fovy);
 end
 
 function ActorDataEvent:run(connection)
@@ -56,6 +59,6 @@ function ActorDataEvent:run(connection)
         Event.send(self, true, connection);
 	end
     if g_spectatorMode ~= nil then
-        g_spectatorMode:actorDataEventF(self.actorName, self.tx, self.ty, self.tz, self.rx, self.ry, self.rz, self.rw);
+        g_spectatorMode:actorDataEventF(self.actorName, self.tx, self.ty, self.tz, self.rx, self.ry, self.rz, self.rw, self.fovy);
     end
 end
