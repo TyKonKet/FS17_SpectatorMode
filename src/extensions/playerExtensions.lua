eca9308eeaa7311e8356dd1b796ac0c7b9deb04c
Matteo Autieri <matteoautieri@live.it>
2017-01-20 14:45:11 +0100

    changed rotation for more precision

--- src/extensions/playerExtensions.lua
+++ src/extensions/playerExtensions.lua
@@ -10,11 +10,13 @@ PlayerExtensions = {};
 function PlayerExtensions:playerWriteStream(streamId, connection)
     --local sm = g_spectatorMode;
     if self.isOwner or (not connection:getIsServer() and connection ~= self.creatorConnection) then
-        local x, y = getRotation(self.cameraNode);
+        local x, y, z, w = getQuaternion(self.cameraNode);
         --sm:print(("playerWriteStream(streamId:%s, connection:%s)[%s]"):format(streamId, connection, self.controllerName));
         --sm:print(("x:%s, y:%s"):format(x, y));
         streamWriteFloat32(streamId, x);
         streamWriteFloat32(streamId, y);
+        streamWriteFloat32(streamId, z);
+        streamWriteFloat32(streamId, w);
     end
 end
 
@@ -23,20 +25,24 @@ function PlayerExtensions:playerReadStream(streamId, connection)
     if not self.isOwner then
         local x = streamReadFloat32(streamId);
         local y = streamReadFloat32(streamId);
+        local z = streamReadFloat32(streamId);
+        local w = streamReadFloat32(streamId);
         --sm:print(("playerReadStream(streamId:%s, connection:%s)[%s]"):format(streamId, connection, self.controllerName));
         --sm:print(("x:%s, y:%s"):format(x, y));
-        setRotation(self.cameraNode, x, y, 0);
+        setQuaternion(self.cameraNode, x, y, z, w);
     end
 end
 
 function PlayerExtensions:playerWriteUpdateStream(streamId, connection, dirtyMask)
     --local sm = g_spectatorMode;
     if self.isOwner or (not connection:getIsServer() and connection ~= self.creatorConnection) then
-        local x, y = getRotation(self.cameraNode);
+        local x, y, z, w = getQuaternion(self.cameraNode);
         --sm:print(("playerWriteUpdateStream(streamId:%s, connection:%s, dirtyMask:%s)[%s]"):format(streamId, connection, dirtyMask, self.controllerName));
         --sm:print(("x:%s, y:%s"):format(x, y));
         streamWriteFloat32(streamId, x);
         streamWriteFloat32(streamId, y);
+        streamWriteFloat32(streamId, z);
+        streamWriteFloat32(streamId, w);    
     end
 end
 
@@ -45,8 +51,10 @@ function PlayerExtensions:playerReadUpdateStream(streamId, timestamp, connection
     if not self.isOwner then
         local x = streamReadFloat32(streamId);
         local y = streamReadFloat32(streamId);
+        local z = streamReadFloat32(streamId);
+        local w = streamReadFloat32(streamId);
         --sm:print(("playerReadUpdateStream(streamId:%s, timestamp:%s, connection:%s)[%s]"):format(streamId, timestamp, connection, self.controllerName));
         --sm:print(("x:%s, y:%s"):format(x, y));
-        setRotation(self.cameraNode, x, y, 0);
+        setQuaternion(self.cameraNode, x, y, z, w);
     end
 end
