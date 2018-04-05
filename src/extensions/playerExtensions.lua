b4a784719e5dbbed2787f1f4d2326d67e225d5c3
TyKonKet <matteoautieri@live.it>
2018-04-06 01:10:28 +0200

    removed spectated player name rendering

--- src/extensions/playerExtensions.lua
+++ src/extensions/playerExtensions.lua
@@ -3,18 +3,18 @@
 --
 -- @author TyKonKet
 -- @date 20/01/2017
-PlayerExtensions = {};
+PlayerExtensions = {}
 
 function PlayerExtensions:writeStream(streamId, connection)
     if not connection:getIsServer() and connection ~= self.creatorConnection then
-        local x, y, z, w = getQuaternion(self.cameraNode);
-        streamWriteFloat32(streamId, x);
-        streamWriteFloat32(streamId, y);
-        streamWriteFloat32(streamId, z);
-        streamWriteFloat32(streamId, w);
-        streamWriteFloat32(streamId, self.camY);
-        streamWriteFloat32(streamId, getFovy(self.cameraNode));
-        streamWriteBool(streamId, g_dedicatedServerInfo and g_currentMission.player.controllerName == self.controllerName);
+        local x, y, z, w = getQuaternion(self.cameraNode)
+        streamWriteFloat32(streamId, x)
+        streamWriteFloat32(streamId, y)
+        streamWriteFloat32(streamId, z)
+        streamWriteFloat32(streamId, w)
+        streamWriteFloat32(streamId, self.camY)
+        streamWriteFloat32(streamId, getFovy(self.cameraNode))
+        streamWriteBool(streamId, g_dedicatedServerInfo and g_currentMission.player.controllerName == self.controllerName)
     end
 end
 
@@ -22,32 +22,32 @@ function PlayerExtensions:readStream(streamId, connection)
     if not self.isOwner and connection:getIsServer() then
         self.lastQuaternion = {
             getQuaternion(self.cameraNode)
-        };
+        }
         self.targetQuaternion = {
             streamReadFloat32(streamId),
             streamReadFloat32(streamId),
             streamReadFloat32(streamId),
             streamReadFloat32(streamId)
-        };
-        self.interpolationAlphaRot = 0;
-        self.camY = streamReadFloat32(streamId);
-        setFovy(self.cameraNode, streamReadFloat32(streamId));
-        self.isDedicatedServer = streamReadBool(streamId);
+        }
+        self.interpolationAlphaRot = 0
+        self.camY = streamReadFloat32(streamId)
+        setFovy(self.cameraNode, streamReadFloat32(streamId))
+        self.isDedicatedServer = streamReadBool(streamId)
     end
 end
 
 function PlayerExtensions:writeUpdateStream(streamId, connection, dirtyMask)
     if not connection:getIsServer() and connection ~= self.creatorConnection then
-        local x, y, z, w = getQuaternion(self.cameraNode);
-        streamWriteFloat32(streamId, x);
-        streamWriteFloat32(streamId, y);
-        streamWriteFloat32(streamId, z);
-        streamWriteFloat32(streamId, w);
-        streamWriteFloat32(streamId, self.camY);
-        streamWriteFloat32(streamId, getFovy(self.cameraNode));
+        local x, y, z, w = getQuaternion(self.cameraNode)
+        streamWriteFloat32(streamId, x)
+        streamWriteFloat32(streamId, y)
+        streamWriteFloat32(streamId, z)
+        streamWriteFloat32(streamId, w)
+        streamWriteFloat32(streamId, self.camY)
+        streamWriteFloat32(streamId, getFovy(self.cameraNode))
     elseif self.isOwner and connection:getIsServer() then
-        streamWriteFloat32(streamId, self.camY);
-        streamWriteFloat32(streamId, getFovy(self.cameraNode));
+        streamWriteFloat32(streamId, self.camY)
+        streamWriteFloat32(streamId, getFovy(self.cameraNode))
     end
 end
 
@@ -55,43 +55,75 @@ function PlayerExtensions:readUpdateStream(streamId, timestamp, connection)
     if not self.isOwner and connection:getIsServer() then
         self.lastQuaternion = {
             getQuaternion(self.cameraNode)
-        };
+        }
         self.targetQuaternion = {
             streamReadFloat32(streamId),
             streamReadFloat32(streamId),
             streamReadFloat32(streamId),
             streamReadFloat32(streamId)
-        };
-        self.interpolationAlphaRot = 0;
-        local cx, _, cz = getTranslation(self.cameraNode);
-        setTranslation(self.cameraNode, cx, streamReadFloat32(streamId), cz);
-        setFovy(self.cameraNode, streamReadFloat32(streamId));
+        }
+        self.interpolationAlphaRot = 0
+        local cx, _, cz = getTranslation(self.cameraNode)
+        setTranslation(self.cameraNode, cx, streamReadFloat32(streamId), cz)
+        setFovy(self.cameraNode, streamReadFloat32(streamId))
     elseif not self.isOwner and not connection:getIsServer() then
-        self.camY = streamReadFloat32(streamId);
-        setFovy(self.cameraNode, streamReadFloat32(streamId));
+        self.camY = streamReadFloat32(streamId)
+        setFovy(self.cameraNode, streamReadFloat32(streamId))
     end
 end
 
 function PlayerExtensions:update(dt)
     if not self.isServer and self.isControlled and not self.isEntered then
-        self.interpolationAlphaRot = self.interpolationAlphaRot + g_physicsDtUnclamped / 75;
+        self.interpolationAlphaRot = self.interpolationAlphaRot + g_physicsDtUnclamped / 75
         if self.interpolationAlphaRot > 1 then
-            self.interpolationAlphaRot = 1;
+            self.interpolationAlphaRot = 1
         end
-        local x, y, z, w = Utils.nlerpQuaternionShortestPath(self.lastQuaternion[1], self.lastQuaternion[2], self.lastQuaternion[3], self.lastQuaternion[4], self.targetQuaternion[1], self.targetQuaternion[2], self.targetQuaternion[3], self.targetQuaternion[4], self.interpolationAlphaRot);
-        setQuaternion(self.cameraNode, x, y, z, w);
+        local x, y, z, w = Utils.nlerpQuaternionShortestPath(self.lastQuaternion[1], self.lastQuaternion[2], self.lastQuaternion[3], self.lastQuaternion[4], self.targetQuaternion[1], self.targetQuaternion[2], self.targetQuaternion[3], self.targetQuaternion[4], self.interpolationAlphaRot)
+        setQuaternion(self.cameraNode, x, y, z, w)
     end
 end
 
 function PlayerExtensions:onEnter(isOwner)
     if isOwner then
         if not g_spectatorMode.spectating then
-            Event.send(CameraChangeEvent:new(g_currentMission.player.controllerName, self.cameraNode, 0, CameraChangeEvent.CAMERA_TYPE_PLAYER));
-            g_spectatorMode:print(string.format("Event.send(CameraChangeEvent:new(controllerName:%s, cameraNode:%s, camIndex:%s, cameraType:%s))", g_currentMission.player.controllerName, self.cameraNode, 0, CameraChangeEvent.CAMERA_TYPE_PLAYER));
+            Event.send(CameraChangeEvent:new(g_currentMission.player.controllerName, self.cameraNode, 0, CameraChangeEvent.CAMERA_TYPE_PLAYER))
+            g_spectatorMode:print(string.format("Event.send(CameraChangeEvent:new(controllerName:%s, cameraNode:%s, camIndex:%s, cameraType:%s))", g_currentMission.player.controllerName, self.cameraNode, 0, CameraChangeEvent.CAMERA_TYPE_PLAYER))
         end
     elseif g_spectatorMode ~= nil then
         if self.controllerName == g_spectatorMode.spectatedPlayer then
-            self:setVisibility(false);
+            self:setVisibility(false)
         end
     end
+end
+
+function PlayerExtensions:drawUIInfo()
+    local spectated = self.isSpectated ~= nil
+    if spectated then
+        spectated = self:isSpectated()
+    end
+    if not spectated then
+        if self.isClient and self.isControlled and not self.isEntered then
+            if not g_gui:getIsGuiVisible() and g_currentMission.showHudEnv then
+                local x, y, z = getTranslation(self.graphicsRootNode)
+                local x1, y1, z1 = getWorldTranslation(getCamera())
+                local diffX = x - x1
+                local diffY = y - y1
+                local diffZ = z - z1
+                local dist = Utils.vector3LengthSq(diffX, diffY, diffZ)
+                if dist <= 100 * 100 then
+                    y = y + self.playerTagYOffset
+                    Utils.renderTextAtWorldPosition(x, y, z, self.controllerName, getCorrectTextSize(0.02), 0)
+                end
+            end
+        end
+    end
+end
+
+function PlayerExtensions:isSpectated()
+    if g_spectatorMode ~= nil then
+        if g_spectatorMode.spectating and self.controllerName == g_spectatorMode.spectatedPlayer then
+            return true
+        end
+    end
+    return false
 end
--- src/spectatorModeRecorder.lua
+++ src/spectatorModeRecorder.lua
@@ -4,121 +4,124 @@
 -- @author TyKonKet
 -- @date 04/01/2017
 SpectatorModeRecorder = {}
-SpectatorModeRecorder.dir = g_currentModDirectory;
-SpectatorModeRecorder.name = "SpectatorModeRecorder";
-SpectatorModeRecorder.debug = true;
+SpectatorModeRecorder.dir = g_currentModDirectory
+SpectatorModeRecorder.name = "SpectatorModeRecorder"
+SpectatorModeRecorder.debug = true
 
 function SpectatorModeRecorder:print(text, ...)
     if self.debug then
-        local start = string.format("[%s(%s)] -> ", self.name, getDate("%H:%M:%S"));
-        local ptext = string.format(text, ...);
-        print(string.format("%s%s", start, ptext));
+        local start = string.format("[%s(%s)] -> ", self.name, getDate("%H:%M:%S"))
+        local ptext = string.format(text, ...)
+        print(string.format("%s%s", start, ptext))
     end
 end
 
 function SpectatorModeRecorder:initialize(missionInfo, missionDynamicInfo, loadingScreen)
-    self = SpectatorModeRecorder;
-    self:print("initialize()");
-    self.isMultiplayer = missionDynamicInfo.missionDynamicInfo.isMultiplayer;
-    self:print("Is Multiplayer : %s", self.isMultiplayer);
+    self = SpectatorModeRecorder
+    self:print("initialize()")
+    self.isMultiplayer = missionDynamicInfo.missionDynamicInfo.isMultiplayer
+    self:print("Is Multiplayer : %s", self.isMultiplayer)
     if not self.isMultiplayer then
-        return;
+        return
     end
-    margeI18N();
-    SpectatorMode.debug = self.debug;
-    SpectatorModeServer.debug = self.debug;
-    g_spectatorMode = SpectatorMode:new(g_server ~= nil, g_client ~= nil);
-    self.spectatorMode = g_spectatorMode;
-    self.spectatorMode.debug = self.debug;
+    margeI18N()
+    SpectatorMode.debug = self.debug
+    SpectatorModeServer.debug = self.debug
+    g_spectatorMode = SpectatorMode:new(g_server ~= nil, g_client ~= nil)
+    self.spectatorMode = g_spectatorMode
+    self.spectatorMode.debug = self.debug
     if g_server ~= nil then
-        self.spectatorMode.server = SpectatorModeServer:new(g_server ~= nil, g_client ~= nil);
-        self.spectatorMode.server.debug = self.debug;
+        self.spectatorMode.server = SpectatorModeServer:new(g_server ~= nil, g_client ~= nil)
+        self.spectatorMode.server.debug = self.debug
     end
-    self.spectatorMode.dir = self.dir;
-    self.spectatorMode.guis = {};
-    self.spectatorMode.guis["spectateGui"] = SpectateGui:new();
-    g_gui:loadGui(self.dir .. "gui/spectateGui.xml", "SpectateGui", self.spectatorMode.guis.spectateGui);
+    self.spectatorMode.dir = self.dir
+    self.spectatorMode.guis = {}
+    self.spectatorMode.guis["spectateGui"] = SpectateGui:new()
+    g_gui:loadGui(self.dir .. "gui/spectateGui.xml", "SpectateGui", self.spectatorMode.guis.spectateGui)
     -- FocusManager restore
-    FocusManager:setGui("MPLoadingScreen");
-    self.fixedUpdateDt = 0;
-    self.fixedUpdateRealDt = 0;
+    FocusManager:setGui("MPLoadingScreen")
+    self.fixedUpdateDt = 0
+    self.fixedUpdateRealDt = 0
     -- extending player functions
-    Player.writeStream = Utils.appendedFunction(Player.writeStream, PlayerExtensions.writeStream);
-    Player.readStream = Utils.appendedFunction(Player.readStream, PlayerExtensions.readStream);
-    Player.writeUpdateStream = Utils.appendedFunction(Player.writeUpdateStream, PlayerExtensions.writeUpdateStream);
-    Player.readUpdateStream = Utils.appendedFunction(Player.readUpdateStream, PlayerExtensions.readUpdateStream);
-    Player.update = Utils.appendedFunction(Player.update, PlayerExtensions.update);
-    Player.onEnter = Utils.appendedFunction(Player.onEnter, PlayerExtensions.onEnter);
+    Player.writeStream = Utils.appendedFunction(Player.writeStream, PlayerExtensions.writeStream)
+    Player.readStream = Utils.appendedFunction(Player.readStream, PlayerExtensions.readStream)
+    Player.writeUpdateStream = Utils.appendedFunction(Player.writeUpdateStream, PlayerExtensions.writeUpdateStream)
+    Player.readUpdateStream = Utils.appendedFunction(Player.readUpdateStream, PlayerExtensions.readUpdateStream)
+    Player.update = Utils.appendedFunction(Player.update, PlayerExtensions.update)
+    Player.onEnter = Utils.appendedFunction(Player.onEnter, PlayerExtensions.onEnter)
+    Player.isSpectated = PlayerExtensions.isSpectated
+    Player.drawUIInfo = PlayerExtensions.drawUIInfo
     -- extending steerable functions
-    Steerable.postLoad = Utils.appendedFunction(Steerable.postLoad, SteerableExtensions.postLoad);
-    Steerable.setActiveCameraIndex = Utils.appendedFunction(Steerable.setActiveCameraIndex, SteerableExtensions.setActiveCameraIndex);
-    Steerable.writeUpdateStream = Utils.appendedFunction(Steerable.writeUpdateStream, SteerableExtensions.writeUpdateStream);
-    Steerable.readUpdateStream = Utils.appendedFunction(Steerable.readUpdateStream, SteerableExtensions.readUpdateStream);
-    Steerable.update = Utils.appendedFunction(Steerable.update, SteerableExtensions.update);
-    Steerable.drawUIInfo = SteerableExtensions.drawUIInfo;
+    Steerable.postLoad = Utils.appendedFunction(Steerable.postLoad, SteerableExtensions.postLoad)
+    Steerable.setActiveCameraIndex = Utils.appendedFunction(Steerable.setActiveCameraIndex, SteerableExtensions.setActiveCameraIndex)
+    Steerable.writeUpdateStream = Utils.appendedFunction(Steerable.writeUpdateStream, SteerableExtensions.writeUpdateStream)
+    Steerable.readUpdateStream = Utils.appendedFunction(Steerable.readUpdateStream, SteerableExtensions.readUpdateStream)
+    Steerable.update = Utils.appendedFunction(Steerable.update, SteerableExtensions.update)
+    Steerable.drawUIInfo = SteerableExtensions.drawUIInfo
+
     -- extending vehicle
-    Vehicle.isSpectated = VehicleExtensions.isSpectated;
+    Vehicle.isSpectated = VehicleExtensions.isSpectated
 end
-g_mpLoadingScreen.loadFunction = Utils.prependedFunction(g_mpLoadingScreen.loadFunction, SpectatorModeRecorder.initialize);
+g_mpLoadingScreen.loadFunction = Utils.prependedFunction(g_mpLoadingScreen.loadFunction, SpectatorModeRecorder.initialize)
 
 function SpectatorModeRecorder:load(missionInfo, missionDynamicInfo, loadingScreen)
-    self = SpectatorModeRecorder;
-    self:print("load()");
+    self = SpectatorModeRecorder
+    self:print("load()")
     if not self.isMultiplayer then
-        return;
+        return
     end
-    g_currentMission.loadMapFinished = Utils.appendedFunction(g_currentMission.loadMapFinished, self.loadMapFinished);
-    g_currentMission.onStartMission = Utils.appendedFunction(g_currentMission.onStartMission, self.afterLoad);
-    g_currentMission.missionInfo.saveToXML = Utils.appendedFunction(g_currentMission.missionInfo.saveToXML, self.saveSavegame);
-    g_currentMission.ingameMap.updatePlayerPosition = self.spectatorMode.updatePlayerPosition;
+    g_currentMission.loadMapFinished = Utils.appendedFunction(g_currentMission.loadMapFinished, self.loadMapFinished)
+    g_currentMission.onStartMission = Utils.appendedFunction(g_currentMission.onStartMission, self.afterLoad)
+    g_currentMission.missionInfo.saveToXML = Utils.appendedFunction(g_currentMission.missionInfo.saveToXML, self.saveSavegame)
+    g_currentMission.ingameMap.updatePlayerPosition = self.spectatorMode.updatePlayerPosition
 end
-g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFunction, SpectatorModeRecorder.load);
+g_mpLoadingScreen.loadFunction = Utils.appendedFunction(g_mpLoadingScreen.loadFunction, SpectatorModeRecorder.load)
 
 function SpectatorModeRecorder:loadMap(name)
-    self:print(("loadMap(name:%s)"):format(name));
+    self:print(("loadMap(name:%s)"):format(name))
     if not self.isMultiplayer then
-        return;
+        return
     end
     if self.spectatorMode ~= nil then
-        self.spectatorMode:loadMap();
+        self.spectatorMode:loadMap()
     end
-    self:loadSavegame();
+    self:loadSavegame()
 end
 
 function SpectatorModeRecorder:loadMapFinished()
-    self = SpectatorModeRecorder;
-    self:print("loadMapFinished()");
+    self = SpectatorModeRecorder
+    self:print("loadMapFinished()")
 end
 
 function SpectatorModeRecorder:afterLoad()
-    self = SpectatorModeRecorder;
-    self:print("afterLoad");
+    self = SpectatorModeRecorder
+    self:print("afterLoad")
     if not self.isMultiplayer then
-        return;
+        return
     end
-    if self.spectatorMode ~= nil then
-        self.spectatorMode:afterLoad();
-    end
+    --if self.spectatorMode ~= nil then
+    --    self.spectatorMode:afterLoad();
+    --end
 end
 
 function SpectatorModeRecorder:loadSavegame()
-    self:print("loadSavegame()");
+    self:print("loadSavegame()")
 end
 
 function SpectatorModeRecorder:saveSavegame()
-    self = SpectatorModeRecorder;
-    self:print("saveSavegame()");
+    self = SpectatorModeRecorder
+    self:print("saveSavegame()")
 end
 
 function SpectatorModeRecorder:deleteMap()
-    self:print("deleteMap()");
+    self:print("deleteMap()")
     if not self.isMultiplayer then
-        return;
+        return
     end
-    if self.spectatorMode ~= nil then
-        self.spectatorMode:deleteMap();
-    end
-    g_spectatorMode = nil;
+    --if self.spectatorMode ~= nil then
+    --    self.spectatorMode:deleteMap();
+    --end
+    g_spectatorMode = nil
 end
 
 function SpectatorModeRecorder:keyEvent(unicode, sym, modifier, isDown)
@@ -129,20 +132,20 @@ end
 
 function SpectatorModeRecorder:update(dt)
     if not self.isMultiplayer then
-        return;
+        return
     end
     if self.spectatorMode ~= nil then
-        self.spectatorMode:update(dt);
+        self.spectatorMode:update(dt)
     end
 end
 
 function SpectatorModeRecorder:draw()
     if not self.isMultiplayer then
-        return;
+        return
     end
     if self.spectatorMode ~= nil then
-        self.spectatorMode:draw();
+        self.spectatorMode:draw()
     end
 end
 
-addModEventListener(SpectatorModeRecorder);
+addModEventListener(SpectatorModeRecorder)
